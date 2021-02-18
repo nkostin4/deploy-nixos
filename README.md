d0d86f979ebbd9ef897e37bc606b3b73ae5a0122
nkostin <nkostin4@protonmail.com>
2021-02-17 20:35:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# deploy-nixos
+
+This repo contains the `configuration.nix` file that deploys a nixos machine.
+
+See the wiki [here](https://nixos.wiki/wiki/St) to see how to get suckless software to work.
---
+++ configuration.nix
@@ -0,0 +1,150 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Enable the Plasma 5 Desktop Environment.
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.autorun = false;
+  services.xserver.displayManager.startx.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nkostin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # suckless dwm
+    (dwm.overrideAttrs (oldAttrs: rec {
+      src = fetchTarball {
+        url = "https://github.com/nkostin4/dwm/archive/master.tar.gz";
+      };
+    }))
+    # suckless slstatus
+    (slstatus.overrideAttrs (oldAttrs: rec {
+      src = fetchTarball {
+        url = "https://github.com/nkostin4/slstatus/archive/master.tar.gz";
+      };
+    }))
+    # suckless dmenu
+    # dmenu
+    (dmenu.overrideAttrs (oldAttrs: rec {
+      src = fetchTarball {
+        url = "https://github.com/nkostin4/dmenu/archive/master.tar.gz";
+      };
+    }))
+    # transfer and fetch files
+    wget
+    curl
+    # version control
+    git
+    # text editor
+    vim
+    notepadqq
+    # terminal emulator
+    rxvt-unicode
+    urxvt_perls
+    urxvt_font_size
+    # interactive process viewer
+    htop
+    # system info script
+    neofetch
+    # suckless image viewer and scrot
+    sxiv
+    scrot
+    # wallpaper and compositor
+    xwallpaper
+    picom
+    # music player daemon and client
+    mpd
+    ncmpcpp
+    # graphical file manager
+    pcmanfm
+    # web browser
+    ungoogled-chromium
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
